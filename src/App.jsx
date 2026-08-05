2ad0664950847de06d0cafdcdf59a9818e7631c3
DeilerCarrillo <wolcfteam1109@gmail.com>
2026-08-05 11:58:39 -0600

    Corregir rutas para github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { Navigate, Route, Routes } from "react-router";
+import {
+  Route,
+  Routes
+} from "react-router";
+
 import Layout from "./components/layout/Layout.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
+
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import ServicesPage from "./pages/ServicesPage.jsx";
@@ -14,22 +19,48 @@ function App() {
     <>
       <ScrollToTop />
 
-     <Routes>
-  <Route element={<Layout />}>
-    <Route index element={<HomePage />} />
-    <Route path="/inicio" element={<HomePage />} />
-    <Route path="/nosotros" element={<AboutPage />} />
-    <Route path="/servicios" element={<ServicesPage />} />
-    <Route path="/catalogo" element={<CatalogPage />} />
-    <Route path="/galeria" element={<GalleryPage />} />
-    <Route path="/cotizacion" element={<QuotePage />} />
-    <Route path="/contacto" element={<ContactPage />} />
-  </Route>
-  <Route path="*" element={<NotFoundPage />} />
-</Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+
+          <Route
+            path="/inicio"
+            element={<HomePage />}
+          />
+
+          <Route
+            path="/nosotros"
+            element={<AboutPage />}
+          />
+
+          <Route
+            path="/servicios"
+            element={<ServicesPage />}
+          />
+
+          <Route
+            path="/catalogo"
+            element={<CatalogPage />}
+          />
+
+          <Route
+            path="/cotizacion"
+            element={<QuotePage />}
+          />
+
+          <Route
+            path="/contacto"
+            element={<ContactPage />}
+          />
+        </Route>
+
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
     </>
   );
 }
 
-export default App; 
-
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
+
 import App from "./App.jsx";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error("No se encontró el elemento #root en index.html.");
+  throw new Error(
+    "No se encontró el elemento #root en index.html."
+  );
 }
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </StrictMode>
 );
